1c6e2618aae733e805a0e75f808a7f0fa9497bf6
Tomas Votroubek <tomas.votroubek@outlook.com>
2023-04-24 03:38:35 +0200

    int to float, clamp acos

--- experiments/rand_parameters.jl
+++ experiments/rand_parameters.jl
@@ -5,8 +5,8 @@ function continuous_uniform(n...; low, high)
 end
 
 function random_manipulator(joint_count; twist_rng, max_angle)
-    r = continuous_uniform(joint_count; low=0.1, high=1)
-    d = continuous_uniform(joint_count; low=0.1, high=1)
+    r = continuous_uniform(joint_count; low=0.1, high=1.0)
+    d = continuous_uniform(joint_count; low=0.1, high=1.0)
     α = twist_rng(joint_count)
 
     θl = vec(fill(-max_angle, joint_count))
@@ -19,16 +19,16 @@ function random_manipulator(joint_count; twist_rng, max_angle)
 end
 
 function params_random_6rad(joint_count)
-	twist_rng(n) = continuous_uniform(n; low=-3, high=3)
-	random_manipulator(joint_count; twist_rng, max_angle=3)
+	twist_rng(n) = continuous_uniform(n; low=-3.0, high=3.0)
+	random_manipulator(joint_count; twist_rng, max_angle=3.0)
 end
 
 function params_random_4rad(joint_count)
-	twist_rng(n) = continuous_uniform(n; low=-3, high=3) # check
-	random_manipulator(joint_count; twist_rng, max_angle=2)
+	twist_rng(n) = continuous_uniform(n; low=-3.0, high=3.0) # check
+	random_manipulator(joint_count; twist_rng, max_angle=2.0)
 end
 
 function params_random_orth(joint_count)
 	twist_rng(n) = rand([-π / 2, π / 2], n)
-	random_manipulator(joint_count; twist_rng, max_angle=3)
+	random_manipulator(joint_count; twist_rng, max_angle=3.0)
 end
--- src/forward_kinematics.jl
+++ src/forward_kinematics.jl
@@ -13,7 +13,7 @@ end
 
 function pose_error(A, B)
 	pos_e = norm(A[1:3, 4] - B[1:3, 4])
-	rot_e = acos(0.5 * (tr(A[1:3,1:3]\B[1:3,1:3])-1))
+	rot_e = acos(clamp(0.5 * (tr(A[1:3,1:3]\B[1:3,1:3])-1), -1.0, 1.0))
 
 	pos_e, rot_e
 end
